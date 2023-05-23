b90be06dfdf34926e3a24ad4e586de4c1e095dde
ivan <izeba1@tvz.hr>
2023-05-23 16:09:25 +0200

    Projektni zadatak 7, obrana oib i drzavljanstvo

--- main_pz7.py
+++ main_pz7.py
@@ -70,6 +70,16 @@ class App(QtWidgets.QMainWindow):
         self.text_prezime = QtWidgets.QLineEdit(self)
         self.text_prezime.setGeometry(QtCore.QRect(150, offset * 5, 150, 25))
 
+        #Label drzavljanstvo
+        self.label_drzavljanstvo = QtWidgets.QLabel(self)
+        self.label_drzavljanstvo.setFont(self.font)
+        self.label_drzavljanstvo.setText('Drzavljanstvo')
+        self.label_drzavljanstvo.move(50, offset * 6)
+
+        # Input drzavljanstvo
+        self.text_drzavljanstvo = QtWidgets.QLineEdit(self)
+        self.text_drzavljanstvo.setGeometry(QtCore.QRect(150, offset * 6, 150, 25))
+
         #Label naziv
         self.label_naziv = QtWidgets.QLabel(self)
         self.label_naziv.setFont(self.font)
@@ -94,18 +104,30 @@ class App(QtWidgets.QMainWindow):
         self.text_web.setGeometry(QtCore.QRect(150, offset * 5, 150, 25))
         self.text_web.hide()
 
+        # Input oib
+        self.label_oib = QtWidgets.QLabel(self)
+        self.label_oib.setFont(self.font)
+        self.label_oib.setText('OIB')
+        self.label_oib.move(50, offset * 6)
+        self.label_oib.hide()
+
+        # Input oib
+        self.text_oib = QtWidgets.QLineEdit(self)
+        self.text_oib.setGeometry(QtCore.QRect(150, offset * 6, 150, 25))
+        self.text_oib.hide()
+
         #Label error
         self.label_error = QtWidgets.QLabel(self)
         self.label_error.setFont(self.font)
         self.label_error.setAlignment(QtCore.Qt.AlignCenter)
         self.label_error.setStyleSheet('color : red')
-        self.label_error.setGeometry(QtCore.QRect(70, offset * 6, 200, 30))
+        self.label_error.setGeometry(QtCore.QRect(70, offset * 7, 200, 30))
 
         #Gumb za unos korisnika
         self.unos_korisnika_button = QtWidgets.QPushButton(self)
         self.unos_korisnika_button.setFont(self.font)
         self.unos_korisnika_button.setText('Unesi korisnika')
-        self.unos_korisnika_button.setGeometry(QtCore.QRect(100, offset * 7, 150, 30))
+        self.unos_korisnika_button.setGeometry(QtCore.QRect(100, offset * 8, 150, 30))
         self.unos_korisnika_button.clicked.connect(self.unos_korisnika)
 
     #Metoda za prikazivanje/sakrivanje labela i inputa
@@ -115,10 +137,14 @@ class App(QtWidgets.QMainWindow):
             self.text_naziv.show()
             self.label_web.show()
             self.text_web.show()
+            self.label_oib.show()
+            self.text_oib.show()
             self.label_ime.hide()
             self.text_ime.hide()
             self.label_prezime.hide()
             self.text_prezime.hide()
+            self.label_drzavljanstvo.hide()
+            self.text_drzavljanstvo.hide()
         if self.tip_korisnika.currentText() == TipKorisnika.PRIVATNI.value:
             self.label_naziv.hide()
             self.text_naziv.hide()
@@ -128,6 +154,8 @@ class App(QtWidgets.QMainWindow):
             self.text_ime.show()
             self.label_prezime.show()
             self.text_prezime.show()
+            self.label_drzavljanstvo.show()
+            self.text_drzavljanstvo.show()
 
     def unos_korisnika(self):
 
@@ -135,7 +163,7 @@ class App(QtWidgets.QMainWindow):
             error_privatni = provjera_korisnickog_unos(self.text_telefon.text(), self.text_email.text(),
                                                        self.text_ime.text() or self.text_naziv, self.text_prezime.text())
             if error_privatni is None:
-                korisnici.append(PrivatniKorisnik(self.text_ime.text(), self.text_prezime.text(),self.text_email.text(),
+                korisnici.append(PrivatniKorisnik(self.text_ime.text(), self.text_prezime.text(), self.text_drzavljanstvo.text(), self.text_email.text(),
                                                   self.text_telefon.text()))
                 self.text_telefon.setText('')
                 self.text_email.setText('')
@@ -143,6 +171,8 @@ class App(QtWidgets.QMainWindow):
                 self.text_web.setText('')
                 self.text_ime.setText('')
                 self.text_prezime.setText('')
+                self.text_drzavljanstvo.setText('')
+                self.text_oib.setText('')
                 self.label_error.setText('')
 
                 korisnik = korisnici[len(korisnici)-1]
@@ -154,7 +184,7 @@ class App(QtWidgets.QMainWindow):
             error_poslovni = provjera_korisnickog_unos(self.text_telefon.text(), self.text_email.text(),
                                                        self.text_naziv.text(), self.text_web.text())
             if error_poslovni is None:
-                korisnici.append(PoslovniKorisnik(self.text_naziv.text(), self.text_web.text(),
+                korisnici.append(PoslovniKorisnik(self.text_naziv.text(), self.text_web.text(), self.text_oib.text(),
                                                   self.text_email.text(), self.text_telefon.text()))
                 self.text_telefon.setText('')
                 self.text_email.setText('')
@@ -162,6 +192,8 @@ class App(QtWidgets.QMainWindow):
                 self.text_web.setText('')
                 self.text_ime.setText('')
                 self.text_prezime.setText('')
+                self.text_drzavljanstvo.setText('')
+                self.text_oib.setText('')
                 self.label_error.setText('')
 
                 korisnik = korisnici[len(korisnici)-1]
